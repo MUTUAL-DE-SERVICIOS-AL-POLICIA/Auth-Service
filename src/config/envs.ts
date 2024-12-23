ee9cc2ae236060b5eaf6da4db78888f08a27833d
ferbautistamollo <ferbautistamollo@gmail.com>
2024-12-23 16:20:15 -0400

    ajustes de configuracion, dockertizacion, base de datos, migraciones y dependencias

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,6 +4,13 @@ import joi from 'joi';
 interface EnvVars {
   PORT: number;
   NATS_SERVERS: string[];
+  DB_PASSWORD: string;
+  DB_DATABASE: string;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USERNAME: string;
+  DB_SYNCHRONIZE: boolean;
+  DB_SCHEMA: string;
   LDAP_AUTHENTICATION: boolean;
   LDAP_HOST: string;
   LDAP_PORT: number;
@@ -29,19 +36,27 @@ const envsSchema = joi
     LDAP_ACCOUNT_SUFFIX: joi.string().required(),
     LDAP_BASEDN: joi.string().required(),
     JWT_SECRET: joi.string().required(),
+    DB_SYNCHRONIZE: joi
+      .string()
+      .valid('true', 'false')
+      .default('false'),
   })
   .unknown(true);
 
 const { error, value } = envsSchema.validate({
   ...process.env,
   NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+  DB_SYNCHRONIZE: process.env.DB_SYNCHRONIZE?.toLowerCase(),
 });
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
 
-const envVars: EnvVars = value;
+const envVars: EnvVars = {
+  ...value,
+  DB_SYNCHRONIZE: value.DB_SYNCHRONIZE === 'true',
+};
 
 export const PortEnvs = {
   port: envVars.PORT,
@@ -65,4 +80,14 @@ export const LdapEnvs = {
 
 export const JwtEnvs = {
   jwtSecret: envVars.JWT_SECRET,
+};
+
+export const DbEnvs = {
+  dbPassword: envVars.DB_PASSWORD,
+  dbDatabase: envVars.DB_DATABASE,
+  dbHost: envVars.DB_HOST,
+  dbPort: envVars.DB_PORT,
+  dbUsername: envVars.DB_USERNAME,
+  dbSynchronize: envVars.DB_SYNCHRONIZE,
+  dbSchema: envVars.DB_SCHEMA,
 };
