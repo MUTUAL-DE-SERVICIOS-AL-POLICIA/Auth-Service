b22e4f25733c481fb6c091c978d5c45673d5212a
gongor7 <crisgongor7@gmail.com>
2025-04-22 15:59:06 -0400

    Importación de entidades

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -13,11 +13,7 @@ export const options: DataSourceOptions & SeederOptions = {
   database: DbEnvs.dbDatabase,
   username: DbEnvs.dbUsername,
   password: DbEnvs.dbPassword,
-  entities: [
-    'dist/persons/entities/**/*.entity.js',
-    'dist/affiliates/entities/**/*.entity.js',
-    'dist/auth/entities/**/*.entity.js',
-  ],
+  entities: [__dirname + '/../**/entities/*.entity{.ts,.js}'],
   synchronize: DbEnvs.dbSynchronize,
   namingStrategy: new SnakeNamingStrategy(),
 
