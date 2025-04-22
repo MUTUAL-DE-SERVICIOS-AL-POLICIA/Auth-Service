d576739f2d5ea9d949acf14028de8661f0a1616a
gongor7 <crisgongor7@gmail.com>
2025-04-22 16:45:28 -0400

    Patrón de mensaje para enviar la lista de usuarios

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
 
-@Controller('user')
-export class UserController {}
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @MessagePattern('get_all_users')
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+}
