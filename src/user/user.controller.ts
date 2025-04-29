a8de1af03ccefadd63a70931a02b4c9d2428378a
gongor7 <crisgongor7@gmail.com>
2025-04-29 10:53:09 -0400

    Patrón de mensaje para obtener los roles de administrador de un usuario

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { UserService } from './user.service';
 import { UserListDto } from './dto/user-list.dto';
 import { UserDetailDto } from './dto/user-detail.dto';
+import { ManagementRoleDto } from './dto/management-rol-dto';
 
 @Controller()
 export class UserController {
@@ -16,5 +17,11 @@ export class UserController {
   @MessagePattern('get_user')
   async findOne(@Payload() data: { uuid: string }): Promise<UserDetailDto> {
     return this.userService.findOne(data.uuid);
+  }
+
+  @MessagePattern('get_user_management_roles')
+  async getUserManagementRoles(userId: any): Promise<ManagementRoleDto[]> {
+    console.log('El usuario autenticado es:', userId.userId);
+    return this.userService.getUserManagementRoles(userId.userId);
   }
 }
