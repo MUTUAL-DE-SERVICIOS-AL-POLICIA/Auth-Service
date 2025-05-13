61fb9db84508f2cd440c0d662f7ef7a31e027230
gongor7 <crisgongor7@gmail.com>
2025-05-13 11:50:13 -0400

    Importación de modulo common

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UserModule],
+  imports: [AuthModule, DatabaseModule, UserModule, CommonModule],
 })
 export class AppModule {}
